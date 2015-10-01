29278dd28b7586c838ecddc48ec6f331fcaacd56
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-01 00:28:31 -0700

    Change button position and label

--- public/controller/lobby.js
+++ public/controller/lobby.js
@@ -12,16 +12,20 @@ lobby.prototype = {
     //gameTitle.anchor.setTo(0.5,0.5);
 
     var style = { font: 'bold 12pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
-    var text = game.add.text(game.world.centerX, game.world.centerY, "-Press Start Below-", style);
+    var text = game.add.text(game.world.centerX, game.world.centerY, "-Press to Start-", style);
     text.anchor.set(0.5);
 
-    buttonleft = game.add.button(0, 30, 'start_button', this.playTheGame, this, 0, 1, 0, 1);
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+
+    buttonleft = game.add.button(game.world.centerX, game.world.centerY-45, 'start_button', this.playTheGame, this, 0, 1, 0, 1);
         buttonleft.events.onInputDown.add(function(){left=true;});
         buttonleft.events.onInputUp.add(function(){left=false;});
+        buttonleft.anchor.set(0.5);
   },
 
   playTheGame: function(){
     alert('button pressed');
-    socket.emit("player ready")
+    socket.emit("player ready");
   }
 };
