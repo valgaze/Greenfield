b8aca9804e5a22de79fdcd18f96e8a807be13f1c
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-02 12:28:33 -0700

    (feat) Implement load entities asynchronously

--- public/client/js/lobby.js
+++ public/client/js/lobby.js
@@ -12,9 +12,16 @@ preload.prototype = {
 
   create: function(){
     socket.on("activate player", function(data){
-      
+      var style = { font: 'bold 12pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
+      var text = game.add.text(game.world.centerX, game.world.centerY, "Loading game...", style);
+      text.anchor.set(0.5);
       //This is the function called when the server is aware that a player pressed "start"
-      game.state.start("Main");
+
+      //Super hacky-- need to come up with triggers to make sure controllers all finish emitting and change state. Without this delay, controllers are synched up only 60% of the time. This is far too long and an undesirable
+      var starter = setTimeout(function(){
+        game.state.start("Main");
+      }, 3000);
+
 
     });
     var gameTitle = this.game.add.sprite(160,160,'title');
