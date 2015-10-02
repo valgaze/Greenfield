b8b3c21e245f4aa03946aa10dfe4e65a3c4c9469
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-02 12:37:23 -0700

    (bug) Add fix for controllers no syncing using a timeout and loading text

--- public/client/js/lobby.js
+++ public/client/js/lobby.js
@@ -17,8 +17,21 @@ preload.prototype = {
 
     socket.on("activate player", function(data){
       
-      //This is the function called when the server is aware that a player pressed "start"
-      game.state.start("Main");
+      var style = {
+         font: 'bold 32pt Arial',
+         fill: 'white',
+         align: 'left',
+         wordWrap: true,
+         wordWrapWidth: 450
+      };
+      var text = game.add.text(game.world.centerX, game.world.centerY, "Loading game...", style);
+      text.anchor.set(0.5);
+
+      /* HACK: need to come up with trigger scheme to make sure controllers all finish emitting and change state. Without this delay, characters are ready only 60% of the time. This delay is too long and an undesirable hack, but loading status text can alleviate in meantime */ 
+
+      var starter = setTimeout(function() {
+         game.state.start("Main");
+      }, 3000);
 
     });
 
