34b8f4cb2ae3de5d7349f9248c15db0bb7c8396f
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-05 15:18:16 -0700

    (Feat) Add new ready button for controller

--- public/controller/lobby.js
+++ public/controller/lobby.js
@@ -3,7 +3,7 @@ var lobby = function(game){};
 lobby.prototype = {
   preload: function(){
     this.game.load.image('title', '../assets/title.png');
-    this.game.load.image('start_button', '../assets/startgamebutton.png');
+    this.game.load.image('ready_button', '../assets/buttonReady.png');
     socket.off("flip controllers");
 
   },
@@ -14,13 +14,13 @@ lobby.prototype = {
     socket.on("reject player", function(){game.state.start("Reject")});
     
     var style = { font: 'bold 12pt Arial', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: 450 };
-    var text = game.add.text(game.world.centerX, game.world.centerY, "-Press to Start-", style);
+    var text = game.add.text(game.world.centerX, game.world.centerY, "-Tap to Join Game-", style);
     text.anchor.set(0.5);
 
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
 
-    buttonleft = game.add.button(game.world.centerX, game.world.centerY-45, 'start_button', this.playTheGame, this, 0, 1, 0, 1);
+    buttonleft = game.add.button(game.world.centerX, game.world.centerY-45, 'ready_button', this.playTheGame, this, 0, 1, 0, 1);
         buttonleft.events.onInputDown.add(function(){left=true;});
         buttonleft.events.onInputUp.add(function(){left=false;});
         buttonleft.anchor.set(0.5);
