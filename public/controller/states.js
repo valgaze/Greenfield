3220c025d99d060add03981192eacb92a223afef
Victor Algaze <valgaze@gmail.comgit config --global user.email valgaze@gmail.com>
2015-10-05 17:23:57 -0700

    (Doc) Document states.js for controller

--- public/controller/states.js
+++ public/controller/states.js
@@ -1,3 +1,12 @@
+/*****************************
+  State setup for controller
+*****************************/
+  //This is where all the views for the controller are connected together
+  //IMPORTANT: this file must be the LAST file added to controller.html so it gets access to all the items above
+  //Listens: Nothing
+  //Emits: Nothing
+
+//Create the game view
 var game = new Phaser.Game(450, 250, Phaser.AUTO, 'gameContainer');
 
 game.state.add("Lobby", lobby);
@@ -5,4 +14,6 @@ game.state.add("Dead", deadState);
 game.state.add("Main", mainState);
 game.state.add("Reject", rejectState);
 game.state.add("Waiting", waitingState);
+
+//Set the starting state
 game.state.start("Lobby");
